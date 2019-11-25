941db343f4754fa8993085d6b3a2a3487850d8f6
Nikola Andric Mitrovic <andricmitrovicnikola@yahoo.com>
2019-11-25 13:47:39 -0800

    changed concept of animation using translation

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ void timer(int);
 
 void init()
 {
-    glClearColor(0.0, 0.0, 1.0, 1.0);
+    glClearColor(0.0, 0.0, 0, 1.0);
 }
 
 int main(int argc, char **argv)
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
     glutMainLoop();
 }
 
-float x_position = -10;
+float x_position = 0;
 int state = 1;
 
 void display()
@@ -36,21 +36,23 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT);  //cistim framebuffer
     glLoadIdentity();              //resetujem matrice transformacija
 
-
-
-    glPointSize(10.0);
+    glTranslatef(x_position,0,0);
 
     glBegin(GL_POLYGON);
 
-    glVertex2f(x_position, 1);
-    glVertex2f(x_position, -1);
-    glVertex2f(x_position+2, -1);
-    glVertex2f(x_position+2, 1);
+    glColor3f(1, 0, 0);
+    glVertex2f(-1, 1);
+
+    glColor3f(1, 1, 0);
+    glVertex2f(-1, -1);
+
+    glColor3f(0, 1, 0);
+    glVertex2f(1, -1);
+
+    glColor3f(0, 0, 1);
+    glVertex2f(1, 1);
 
     glEnd();
-
-
-
 
 
 
@@ -81,13 +83,13 @@ void timer(int)
     switch(state)
     {
         case(1):
-            if(x_position<8)
+            if(x_position<9)
                 x_position+=0.15;
             else
                 state = -1;
             break;
         case(-1):
-            if(x_position>-10)
+            if(x_position>-9)
                 x_position-=0.15;
             else
                 state = 1;

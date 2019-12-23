23a580ac3f0f88919366f5234b1706477f6d50f1
Nikola Andric Mitrovic <andricmitrovicnikola@yahoo.com>
2019-12-23 11:44:19 -0800

    Added different reshape funcs depending on display mode, added lightning and sphere changed to solid, added func for drawing axis

--- main.cpp
+++ main.cpp
@@ -28,6 +28,24 @@ void init()
     glClearColor(0.3, 0.7, 0.3, 1.0);
     glShadeModel (GL_SMOOTH);
 
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_NORMALIZE);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    float light_position[] = {-1, 3, 2, 1};
+    float light_ambient[] = {.3f, .3f, .3f, 1};
+    float light_diffuse[] = {.7f, .7f, .7f, 1};
+    float light_specular[] = {.7f, .7f, .7f, 1};
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+
+    glEnable(GL_COLOR_MATERIAL);
+
     x_position = 0;
     y_position = 0;
     player_size = 2;
@@ -55,8 +73,32 @@ int main(int argc, char **argv)
     glutMainLoop();
 }
 
+void draw_axes(float len) {
+    glDisable(GL_LIGHTING);
+
+    glBegin(GL_LINES);
+        glColor3f(1,0,0);
+        glVertex3f(0,0,0);
+        glVertex3f(len,0,0);
+
+        glColor3f(0,1,0);
+        glVertex3f(0,0,0);
+        glVertex3f(0,len,0);
+
+        glColor3f(0,0,1);
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,len);
+    glEnd();
+
+    glEnable(GL_LIGHTING);
+}
+
 void display1()
 {
+    gluLookAt(0, 0, 1000, 0, 0, 0, 0, 1, 0);
+
+    glDisable(GL_LIGHTING);
+
     glClear(GL_COLOR_BUFFER_BIT);  
     glLoadIdentity();              
 
@@ -85,12 +127,19 @@ void display1()
 
 void display2()
 {
+    gluLookAt(4, 4, 4,
+              0, 0, 0,
+              0, 1, 0);
+
+    glEnable(GL_LIGHTING);
     glClear(GL_COLOR_BUFFER_BIT);  
     glLoadIdentity();
 
+    draw_axes(5);
+
     glColor3f(1, 0, 0);
     glRotatef(60, 1, 1, 1);
-    glutWireSphere(2, 25, 25);
+    glutSolidSphere(2, 25, 25);
 
     glutSwapBuffers();                    
 }
@@ -98,8 +147,6 @@ void display2()
 
 void display()
 {
-    gluLookAt(0, 0, 1000, 0, 0, 0, 0, 1, 0);
-
     if(window_select == WINDOW_FIELD)
     {
         display1();
@@ -112,6 +159,20 @@ void display()
 
 void reshape(int w, int h)
 {
+
+    if(window_select == WINDOW_FIELD)
+    {
+        reshape1(w, h);
+    }
+    else if(window_select == WINDOW_POKEBALL)
+    {
+        reshape2(w, h);
+    }
+
+}
+
+void reshape1(int w, int h)
+{
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -119,8 +180,19 @@ void reshape(int w, int h)
     glOrtho(-10, 10, -10, 10, -10, 10);
 
     glMatrixMode(GL_MODELVIEW);
-
 }
+
+void reshape2(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective(30, (float) w/h, 1, 20);
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
 
 void timer(int)
 {

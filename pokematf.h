3e7702ecc145fe612cee3ab6f27e854917737ead
Nikola Andric Mitrovic <andricmitrovicnikola@yahoo.com>
2019-12-23 12:06:07 -0800

    Separated code into main, source and header file

---
+++ pokematf.h
@@ -0,0 +1,35 @@
+#ifndef POKEMATF_H
+#define POKEMATF_H
+
+#include <GL/glut.h>
+#include <iostream>
+
+
+#define WINDOW_FIELD 0
+#define WINDOW_POKEBALL 1
+
+extern float x_position;
+extern float y_position;
+extern float player_size;
+extern int state;
+extern bool window_select;
+
+void draw_axes(float len);
+
+void display1();
+
+void display2();
+
+void display();
+
+void reshape(int w, int h);
+
+void reshape1(int w, int h);
+
+void reshape2(int w, int h);
+
+void timer(int);
+
+void keyboard(unsigned char key, int x, int y);
+
+#endif
